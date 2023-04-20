33111f536ae9d733a78da6e7eeff219f509decf2
Frank <f.neumann.berlin@googlemail.com>
2023-04-20 11:21:26 +0200

    mit user insert und select

--- app/src/main/java/de/softdeveloper/firestoredemo/MainActivity.kt
+++ app/src/main/java/de/softdeveloper/firestoredemo/MainActivity.kt
@@ -2,15 +2,93 @@ package de.softdeveloper.firestoredemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import de.softdeveloper.firestoredemo.databinding.ActivityMainBinding
+
+data class User(
+    val first:String,
+    val last:String,
+    val city: String,
+    val age: Int
+)
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        db = Firebase.firestore
+
+        val userCollection = db.collection("users")
+
+        userCollection.get()
+            .addOnCompleteListener {task ->
+                if (task.isSuccessful){
+                    for(document in task.result){
+                        binding.tvOutput.append("User: ${document.id}, ${document.data} name: ${document.get("first")} \n")
+                    }
+                }
+            }
+        val u1 = User("frank","Neumann","Berlin",53)
+        val u2 = User("peter","Meier","Hamburg",45)
+        val u3 = User("Horst","Schulze","Berlin",60)
+        val u4 = User("Günter","Lehmann","München",31)
+
+        
+
+        val map = mapOf(
+            "1" to u1,
+            "2" to u2,
+            "3" to u3,
+            "4" to u4,
+        )
+
+//        db.collection("users").apply {
+//            map.forEach  {key,user ->
+//                document(key).set(user)
+//
+//            }
+//        }
+        binding.tvOutput.text =""
+        userCollection.whereGreaterThan("age",50).get()
+            .addOnSuccessListener {
+                it.forEach {
+                    binding.tvOutput.append(it.get("last").toString() + "\n")
+                }
+            }
+    }
+
+    fun createUsersDummy(){
+        var user = hashMapOf(
+            "first" to "Frank"
+        )
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener {
+                Toast.makeText(this, "User wurde angelegt", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            }
+
+        var user2 = hashMapOf(
+            "first" to "Hans",
+            "last" to "Müller",
+            "city" to "Bonn",
+            "age" to 23,
+            "isAdult" to true
+        )
+
+        db.collection("users")
+            .add(user2)
+            .addOnFailureListener { Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show() }
     }
 }
